e2d1cef1f566f7c00f458a5598adb5c5337babc8
usamaa1 <usamariz1997@gmail.com>
2025-05-08 13:09:49 +0500

    till for loop table

--- program.cs
+++ program.cs
@@ -35,37 +35,127 @@
 
 
 
-Console.WriteLine("Please Enter your Age");
-String userAge = Console.ReadLine();
+//Console.WriteLine("Please Enter your Age");
+//String userAge = Console.ReadLine();
 
-if(int.Parse(userAge) > 18)
-{
-    Console.WriteLine("You are Eligible for vote");
-}
-else if (int.Parse(userAge) == 18)
-{
-   Console.WriteLine("Are you married");
-   Console.WriteLine("If yes Press 'Y', if no press 'N'");
-   String userMarriageStatus = Console.ReadLine();
-   String capitalUserMarriageStatus =  userMarriageStatus.ToUpper();
+//if(int.Parse(userAge) > 18)
+//{
+//    Console.WriteLine("You are Eligible for vote");
+//}
+//else if (int.Parse(userAge) == 18)
+//{
+//   Console.WriteLine("Are you married");
+//   Console.WriteLine("If yes Press 'Y', if no press 'N'");
+//   String userMarriageStatus = Console.ReadLine();
+//   String capitalUserMarriageStatus =  userMarriageStatus.ToUpper();
 
-    if(capitalUserMarriageStatus == "Y")
+//    if(capitalUserMarriageStatus == "Y")
+//    {
+//        Console.WriteLine("Congratulation You are eligible for vote");
+//    }
+//    else if(capitalUserMarriageStatus == "N")
+//    {
+//        Console.WriteLine("Sorry you are not eligible for vote");
+//    }
+//    else
+//    {
+//        Console.WriteLine("Please give your answer in 'Y' or 'N'");
+//    }
+
+
+
+//}
+//else
+//{
+//    Console.WriteLine("You are not elgible for vote");
+//}
+
+
+
+
+// LOOPS
+
+// i = 1 + 1  //2 //i++
+// i = i + 3 //4 //i+=3
+
+
+//For
+//for (int i = 1; i <= 100; i++)
+//{
+//    Console.WriteLine(i);
+//}
+//reverse counting
+//for (int i = 100; i >= 1; i--)
+//{
+//    Console.WriteLine(i);
+//}
+
+//even
+//for (int i = 2; i <= 100; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+//odd
+//for (int i = 1; i <= 100; i += 2)
+//{
+//    Console.WriteLine(i);
+//}
+//5 table
+//for (int i = 5; i <= 50; i += 5)
+//{
+//    Console.WriteLine(i);
+//}
+
+
+for (int i = 0; i <= 40; i++)
+{
+    if( i%2 == 0)
     {
-        Console.WriteLine("Congratulation You are eligible for vote");
-    }
-    else if(capitalUserMarriageStatus == "N")
-    {
-        Console.WriteLine("Sorry you are not eligible for vote");
+        Console.WriteLine("This is Even Number: "+i);
     }
     else
     {
-        Console.WriteLine("Please give your answer in 'Y' or 'N'");
+        Console.WriteLine("This is Odd Number: " + i);
     }
 }
-else
-{
-    Console.WriteLine("You are not elgible for vote");
-}
+
+
+
+// 2 x 1 = 2
+// 2 x 2 = 4
+// 2 x 3 = 6
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
