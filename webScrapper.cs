88ecba0fbbbcb04b30cf79d6b8746e366715ee65
usamaa1 <usamariz1997@gmail.com>
2025-06-05 12:58:27 +0500

    till scrapper

--- webScrapper.cs
+++ webScrapper.cs
@@ -1,4 +1,16 @@
 // See https://aka.ms/new-console-template for more information
+
+// ______________________________________________________________________________________________
+
+// USEFUL link for scrapper
+
+// To add text to file
+// https://learn.microsoft.com/en-us/dotnet/standard/io/how-to-write-text-to-a-file
+
+// to scrape data of web
+// https://learn.microsoft.com/en-us/dotnet/api/system.net.webclient.downloadstring?view=net-9.0
+
+// ______________________________________________________________________________________________
 using System.Net;
 
 Console.WriteLine("Give me Website URL:");
